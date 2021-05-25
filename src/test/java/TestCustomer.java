4abf8107ed56db6ef401be0830e5609c2476e9f3
MilanVDZ <Milan.VanderZypen@student.intecbrussel.be>
2021-05-25 14:26:12 +0200

    FridayNight

--- src/test/java/TestCustomer.java
+++ src/test/java/TestCustomer.java
@@ -1,4 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestCustomer {
 
     // DENIS
+
+    @Test
+    public void feelingConfident(){
+        Customer denis = new Customer("denis",10,49,true,true);
+        denis.setDrinksThatTheyDrunk(7);
+        denis.feelingConfident();
+        Assertions.assertTrue(denis.feelingConfident());
+    }
+
+    @Test
+    public void tipTheCuteStaff(){
+        BarTender barTender = new BarTender("denis",2.50,5);
+        Customer denis = new Customer("denis",10,50,true,false);
+        denis.setDrinksThatTheyDrunk(7);
+        denis.tipTheCuteStaff(barTender);
+        Assertions.assertEquals(denis.getDrinksThatTheyDrunk()*1.50,barTender.getAmountsOfTips());
+
+    }
 }
--- src/test/java/TestToiletman.java
+++ src/test/java/TestToiletman.java
@@ -1,4 +1,26 @@
-public class TestToiletman {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class TestToiletMan {
 
-    // GREGORY
+    //GREGORY
+
+    @Test
+    public void fillToiletStallsWithToiletPaperTest() throws Exception {
+        ToiletMan toiletman = new ToiletMan("greg", 5);
+        toiletman.fillToiletStallsWithToiletPaper(1);
+        Assertions.assertEquals(4, toiletman.getToiletPaperStock());
+        Assertions.assertThrows(Exception.class, () -> {
+                    toiletman.fillToiletStallsWithToiletPaper(5);
+                }
+        );
+    }
+
+    @Test
+    public void promoteSafeFunTest(){
+        Customer customer = new Customer("greg",10,1,true,false);
+        customer.setDrinksThatTheyDrunk(2);
+        ToiletMan toiletMan = new ToiletMan("greg", 2);
+        toiletMan.promoteSafeFun(customer);
+        Assertions.assertTrue(customer.isHasCondom());
+    }
 }
